a22d23845b40d9fbe3cda0a164a89ae14da99a58
AtharvNarale07 <atharvnarale5@gmail.com>
2025-10-04 14:09:24 +0530

    Completed orders module (admin order display + frontend tracking)

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -43,17 +43,46 @@ const placeOrderRazorpay = async (req,res) => {
 
 // All Orders data for Admin Panel
 const allOrders = async (req,res) => {
+    try {
+        
+        const orders = await orderModel.find({});
+        res.json({success:true, orders});
 
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
 } 
 
 // User Order Data for frontend
 const userOrders = async (req,res) => {
+    try {
 
+        const { userId } = req.body;
+
+        const orders = await orderModel.find({ userId });
+        res.json({success:true, orders});
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message});
+    }
 }
 
 // Update order status from Admin Panel
 const updateStatus = async (req,res) => {
-     
+
+    try {
+        
+        const { orderId, status } = req.body;
+
+        await orderModel.findByIdAndUpdate(orderId, {status});
+        res.json({success:true, message:'Status Updated'});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message});
+    }
 }
 
 export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus }
