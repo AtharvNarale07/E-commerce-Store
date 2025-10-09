b9567bebe9b4b302d0f9f18287329687b4f36804
AtharvNarale07 <atharvnarale5@gmail.com>
2025-10-09 12:12:26 +0530

    Integrate Stripe payment flow with frontend and add order & payment verification

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -72,7 +72,18 @@ const PlaceOrder = () => {
           }
           
           break;
-      
+        
+        case 'stripe':
+          const responseStripe = await axios.post(backendUrl + '/api/order/stripe', orderData, {headers:{token}});
+          if (responseStripe.data.success) {
+            const {session_url} = responseStripe.data;
+            window.location.replace(session_url);
+          }
+          else {
+            toast.error(responseStripe.data.message);
+          }
+          break;
+
         default:
           break;
       }
